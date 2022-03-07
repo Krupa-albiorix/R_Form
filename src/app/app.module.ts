59c30f2e10da7993d9cd6fd92ac0a429fac8a2e4
Krupa Patel <53213815+krupa253@users.noreply.github.com>
2022-03-07 19:43:21 +0530

    code started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './home/form/form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/form/form.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ConfirmedValidator } from '../../Validator/confirmed.validator';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  title = "Registration Form";
+
+  registrationForm! : FormGroup;
+  user : any = {};
+
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
+
+  createRegistrationForm() {
+    this.registrationForm = this.formBuilder.group({
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(200)]],
+      hobbies: ['', [Validators.required]],
+      mobile: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      date: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern("^([a-zA-Z0-9@*#]{8,15})$")]],
+      cpassword: ['', [Validators.required]]
+    }, { 
+      validator: ConfirmedValidator('password', 'cpassword')
+    });
+  }
+
+  ngOnInit(): void {
+    this.createRegistrationForm();
+  }
+  onClickSubmit() {
+    console.log(this.registrationForm.value);
+    this.user = Object.assign(this.user, this.registrationForm.value);
+    this.addUser(this.user);
+  }
+
+  addUser(user: any) {
+    let users = [];
+    if (localStorage.getItem('Users')) {
+      users = JSON.parse(localStorage.getItem('Users')!);
+      users = [user, ...users];
+    }
+    else {
+      users = [user];
+    }
+    localStorage.setItem('Users', JSON.stringify(users));
+  }
+
+  // onClickSubmit(data : any) {
+  //   this.myitem = localStorage.getItem(data);
+  //   console.log(this.myitem);
+  // }
+  // key(key: any): any {
+  //   throw new Error('Method not implemented.');
+  // }
+
+  formSubmit() {
+    console.log(this.registrationForm)
+  }
+
+  hobbies = [
+    { id : 1, name : 'Reading' },
+    { id : 2, name : 'Cooking' },
+    { id : 3, name : 'Playing Cricket' },
+    { id : 4, name : 'Listening Music' }
+  ];
+
+}
